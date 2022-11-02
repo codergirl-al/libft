9cbad4be8f5ae01f62d12ca9df3bc30c6e049674
Albina Peposhi <apeposhi@2-B-3.42heilbronn.de>
2022-11-02 18:51:05 +0100

    Small fix

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,23 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 14:29:07 by apeposhi          #+#    #+#             */
-/*   Updated: 2022/10/11 14:29:08 by apeposhi         ###   ########.fr       */
+/*   Updated: 2022/11/02 18:31:16 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (ft_strdup(""));
+	if (18446744073709551615UL / size < nmemb)
+		return (NULL);
+	ptr = (void *)malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, nmemb * size);
+	return (ptr);
+}
