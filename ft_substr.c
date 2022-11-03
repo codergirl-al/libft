2d5ae2d83f70e30b9610cc920ef8972cfc92b25e
Albina Peposhi <apeposhi@3-C-8.42heilbronn.de>
2022-11-03 15:46:18 +0100

    Working

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 18:40:24 by apeposhi          #+#    #+#             */
-/*   Updated: 2022/11/02 18:41:03 by apeposhi         ###   ########.fr       */
+/*   Updated: 2022/11/03 15:44:12 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	
+	size_t	i;
+	size_t	j;
+	char	*subs;
+
+	if (!s || !len)
+		return (NULL);
+	subs = (char *)malloc(len + 1);
+	if (!subs)
+		return (NULL);
+	i = (size_t)start;
+	j = 0;
+	while (j < len && i < ft_strlen(s))
+	{
+		subs[j] = s[i];
+		i++;
+		j++;	
+	}
+	subs[j] = '\0';
+	return (subs);
 }
