7a7abe49070027f762dc41dd4db19f57fbdd7f5a
Albina Peposhi <apeposhi@1-B-27.42heilbronn.de>
2022-11-04 14:47:53 +0100

    Added substr function

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 18:40:24 by apeposhi          #+#    #+#             */
-/*   Updated: 2022/11/04 12:31:32 by apeposhi         ###   ########.fr       */
+/*   Updated: 2022/11/04 14:24:29 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	char	*subs;
 
-	if (!s || !(subs = malloc(sizeof(char) * (len + 1))))
+	subs = malloc(sizeof(char) * (len + 1));
+	if (!s || !subs)
 		return (NULL);
 	i = 0;
 	while (start < ft_strlen((char *)s) && s[start + i] && i < len)
@@ -25,6 +26,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		subs[i] = s[start + i];
 		i++;
 	}
-	subs[i] = '\0';
+	subs[i] = 0;
 	return (subs);
 }
