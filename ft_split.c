eaf184370fa34d30066f936547c3b406ae5981db
Albina Peposhi <apeposhi@3-E-6.42heilbronn.de>
2022-11-20 15:57:29 +0100

    Aded split

--- ft_split.c
+++ ft_split.c
@@ -6,39 +6,70 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 03:43:56 by apeposhi          #+#    #+#             */
-/*   Updated: 2022/11/17 12:47:53 by apeposhi         ###   ########.fr       */
+/*   Updated: 2022/11/20 15:26:42 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	count_words(const char *str, char c)
+{
+	int	i;
+	int	trigger;
+
+	i = 0;
+	trigger = 0;
+	while (*str)
+	{
+		if (*str != c && trigger == 0)
+		{
+			trigger = 1;
+			i++;
+		}
+		else if (*str == c)
+			trigger = 0;
+		str++;
+	}
+	return (i);
+}
+
+static char	*word_dup(const char *str, int start, int finish)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc((finish - start + 1) * sizeof(char));
+	while (start < finish)
+		word[i++] = str[start++];
+	word[i] = '\0';
+	return (word);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	size_t	i;
 	size_t	j;
-	size_t	k;
-	char	**arr[ft_strlen(s)];
+	int		index;
+	char	**split;
 
+	split = malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!s || !split)
+		return (0);
 	i = 0;
-	k = 0;
-	while (i < ft_strlen(s))
+	j = 0;
+	index = -1;
+	while (i <= ft_strlen(s))
 	{
-		if (ft_strchr(s, i))
-			i++;
-		else
+		if (s[i] != c && index < 0)
+			index = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
 		{
-			j = i;
-			while (j < ft_strlen(s))
-			{
-				if (s[j] == c)
-				{
-					(char *)(arr + k) = ft_substr(s, i, j - i + 1);
-					k++;
-				}
-				j++;
-			}
+			split[j++] = word_dup(s, index, i);
+			index = -1;
 		}
 		i++;
 	}
-	return arr;
+	split[j] = 0;
+	return (split);
 }
