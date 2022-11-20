58dfaa5bd2235896e08c6194cc55c5faa6fb6156
Albina Peposhi <apeposhi@3-E-6.42heilbronn.de>
2022-11-20 15:57:37 +0100

    Added itoa

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 16:24:48 by apeposhi          #+#    #+#             */
-/*   Updated: 2022/11/17 03:44:27 by apeposhi         ###   ########.fr       */
+/*   Updated: 2022/11/20 15:52:55 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
