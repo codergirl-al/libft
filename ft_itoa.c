08de9ea42b7d3c71e0aecc03b19b5afea6a7369f
Albina Peposhi <apeposhi@3-E-6.42heilbronn.de>
2022-11-20 16:37:52 +0100

    Added itoa funct

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 15:52:17 by apeposhi          #+#    #+#             */
-/*   Updated: 2022/11/20 15:54:13 by apeposhi         ###   ########.fr       */
+/*   Updated: 2022/11/20 16:37:41 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,27 @@ char	*ft_itoa(int n)
 {
 	char	*str;
 	long	nbr;
+	size_t	size;
+
+	nbr = n;
+	size = n > 0 ? 0 : 1;
+	nbr = nbr > 0 ? nbr : -nbr;
+	while (n)
+	{
+		n /= 10;
+		size++;
+	}
+	if (!(str = (char *)malloc(size + 1)))
+		return (0);
+	*(str + size--) = '\0';
+	while (nbr > 0)
+	{
+		*(str + size--) = nbr % 10 + '0';
+		nbr /= 10;
+	}
+	if (size == 0 && str[1] == '\0')
+		*(str + size) = '0';
+	else if (size == 0 && str[1] != '\0')
+		*(str + size) = '-';
+	return (str);
 }
