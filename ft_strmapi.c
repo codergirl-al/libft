8dbdf1531eac51b547259ce5a92ae3f2f94180a4
Albina Peposhi <apeposhi@1-B-21.42heilbronn.de>
2022-11-23 17:32:52 +0100

    Added strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,11 +6,30 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 15:59:40 by apeposhi          #+#    #+#             */
-/*   Updated: 2022/11/22 16:51:14 by apeposhi         ###   ########.fr       */
+/*   Updated: 2022/11/23 15:21:26 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	size_t	i;
+	size_t	len;
+	char	*new_s;
+
+	if (!s || !f)
+		return (NULL);
+	i = 0;
+	len = ft_strlen(s);
+	new_s = malloc(sizeof(char) * (len + 1));
+	if (!new_s)
+		return (NULL);
+	while (i < len)
+	{
+		new_s[i] = f(i, s[i]);
+		i++;
+	}
+	new_s[i] = '\0';
+	return (new_s);
 }
