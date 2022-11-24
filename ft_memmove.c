3896f59ddf0f23d90d1c703388f67565d6cbd256
Albina Peposhi <apeposhi@1-B-31.42heilbronn.de>
2022-11-24 11:14:01 +0100

    Fixed norm issues

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 14:49:28 by apeposhi          #+#    #+#             */
-/*   Updated: 2022/10/28 17:26:55 by apeposhi         ###   ########.fr       */
+/*   Updated: 2022/11/24 11:06:43 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,16 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*d;
-	char	*s;
 	int		i;
 
-	d = (char *)dest;
-	s = (char *)src;
 	if (!dest && !src)
 		return (NULL);
-	
-	if (d > s)
+	if (dest > src)
 	{
 		i = (int)n - 1;
-		while (i >= 0 && dest != NULL)
+		while (i >= 0)
 		{
-			d[i] = s[i];
+			*(char *)(dest + i) = *(char *)(src + i);
 			i--;
 		}
 	}
@@ -37,29 +32,9 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		i = 0;
 		while (i < (int)n && src != NULL)
 		{
-			d[i] = s[i];
+			*(char *)(dest + i) = *(char *)(src + i);
 			i++;
 		}	
 	}
 	return (dest);
 }
-
-// #include <stdio.h>
-// #include <string.h>
-// int main()
-// {
-// 	char s1[100];
-// 	for (int i = 0; i < 100; ++i)
-// 	{
-// 		s1[i] = (char)i % 26 + 'a';
-// 	}
-// 	printf("%s\n", s1);
-// 	ft_memmove(s1, s1 + 99, 1);
-// 	printf("%s\n", s1);
-// 	for (int i = 0; i < 100; ++i)
-// 	{
-// 		s1[i] = (char)i % 26 + 'a';
-// 	}
-// 	memmove(s1, s1 + 99, 1);
-// 	printf("%s\n", s1);
-// }
