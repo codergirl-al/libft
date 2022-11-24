ee7c86d5115bfeb3e2168b192591719b4526b8ef
Albina Peposhi <apeposhi@1-B-31.42heilbronn.de>
2022-11-24 17:46:35 +0100

    Added funcs

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/17 14:25:17 by apeposhi          #+#    #+#              #
-#    Updated: 2022/11/23 17:40:26 by apeposhi         ###   ########.fr        #
+#    Updated: 2022/11/24 17:20:30 by apeposhi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -30,6 +30,10 @@ ft_putchar_fd.c	ft_putstr_fd.c	ft_putendl_fd.c	ft_putnbr_fd.c
 
 OBJS=$(SRCS:.c=.o)
 
+BONUSES=ft_lstnew.c		ft_lstadd_front.c		ft_lstsize.c	ft_lstlast.c	ft_lstadd_back.c
+
+BONUS_OBJS = $(BONUSES:.c=.o)
+
 all: $(NAME)
 
 %.o: %.c
@@ -38,8 +42,11 @@ all: $(NAME)
 $(NAME): $(SRCS) $(OBJS)
 	@ar rcs $(NAME) $(OBJS)
 
+bonus:	$(OBJS) $(BONUS_OBJS)
+	ar rcs $(NAME) $(OBJS) $(BONUS_OBJS)
+
 clean:
-	rm -f $(OBJS)
+	rm -f $(OBJS) $(BONUS_OBJS)
 
 fclean:
 	rm -f $(NAME)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 16:24:48 by apeposhi          #+#    #+#             */
-/*   Updated: 2022/11/24 10:13:25 by apeposhi         ###   ########.fr       */
+/*   Updated: 2022/11/24 17:17:13 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -50,5 +56,10 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
