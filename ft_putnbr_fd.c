ce4063b4743975f8cc9b4d0c2a577f64eea643b4
Albina Peposhi <apeposhi@1-B-31.42heilbronn.de>
2022-11-24 11:14:14 +0100

    Added putnbr func

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 17:38:54 by apeposhi          #+#    #+#             */
+/*   Updated: 2022/11/24 10:05:44 by apeposhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_val(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	str[13];
+	int		is_neg;
+	int		length;
+
+	is_neg = (n < 0);
+	ft_bzero(str, 13);
+	if (n == 0)
+		str[0] = '0';
+	length = 0;
+	while (n != 0)
+	{
+		str[length++] = '0' + ft_val(n % 10);
+		n = (n / 10);
+	}
+	if (is_neg)
+		str[length] = '-';
+	else if (length > 0)
+		length--;
+	while (length >= 0)
+		write(fd, &str[length--], 1);
+}
