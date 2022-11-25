c6a93ca28bdb70c7a28e384fcaeddf1b28699182
Albina Peposhi <apeposhi@1-B-21.42heilbronn.de>
2022-11-25 12:21:53 +0100

    Added lstdelone function

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,15 +6,19 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 18:52:47 by apeposhi          #+#    #+#             */
-/*   Updated: 2022/11/24 17:45:19 by apeposhi         ###   ########.fr       */
+/*   Updated: 2022/11/25 12:10:11 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h" 
 
-// void    ft_lstdelone(t_list *lst, void (*del)(void *))
-// {
-//     if (!lst || !del)
-//         return ;
-//     del((void *)lst);
-// }
+void    ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+    if (!del)
+        return ;
+    if (lst)
+	{
+		(*del)(lst->content);
+		free(lst);
+	}
+}
