1adb86ef89aa5931113672ca2a01a01825f46279
Albina Peposhi <apeposhi@1-B-21.42heilbronn.de>
2022-11-25 13:06:38 +0100

    Added lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/25 12:22:58 by apeposhi          #+#    #+#             */
+/*   Updated: 2022/11/25 13:05:18 by apeposhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*temp;
+
+	if (!del || !lst || !*lst)
+		return ;
+	while (lst && *lst)
+	{
+		temp = (*lst) -> next;
+		ft_lstdelone(*lst, del);
+		*lst = temp;
+	}
+}
