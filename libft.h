8c48a92dbcf1e1aa37c5a15a30510ab2705f4765
Albina Peposhi <apeposhi@1-B-21.42heilbronn.de>
2022-11-25 12:21:37 +0100

    added funcs

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 16:24:48 by apeposhi          #+#    #+#             */
-/*   Updated: 2022/11/24 17:17:13 by apeposhi         ###   ########.fr       */
+/*   Updated: 2022/11/25 12:09:34 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void    ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
